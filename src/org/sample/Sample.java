2f46a220ef22db6786979402ca5ce6682341068c
king3311 <d76493315@gmail.com>
2022-11-25 21:34:34 +0530

    Created

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,19 @@
+package org.sample;
+
+public class Sample {
+	
+      private void project1() {
+		System.out.println("11");
+    	  	  
+	}
+     private void project2() {
+    	 System.out.println("22");
+	}	
+     private void project3() {
+ 		System.out.println("33");
+     	  	  
+ 	} 
+      
+      
+ }
+	
