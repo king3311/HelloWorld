5290e467ce1ced5c50e1f869566c86aa30da60a9
king3311 <d76493315@gmail.com>
2022-11-25 21:44:19 +0530

    second

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,19 +1,24 @@
 package org.sample;
 
 public class Sample {
-	
-      private void project1() {
+
+	private void project1() {
 		System.out.println("11");
-    	  	  
+
 	}
-     private void project2() {
-    	 System.out.println("22");
-	}	
-     private void project3() {
- 		System.out.println("33");
-     	  	  
- 	} 
-      
-      
- }
-	
+
+	private void project2() {
+		System.out.println("22");
+	}
+
+	private void project3() {
+		System.out.println("33");
+
+	}
+
+	private void project4() {
+		System.out.println("44");
+
+	}
+
+}
